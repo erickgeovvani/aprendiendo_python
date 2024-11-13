5f1f6ef78996e384d73340012d8c7756fb453671
Erick Geovvani <erickgeo98@gmail.com>
2024-11-12 16:02:26 -0800

    TEST

--- Comentarios.py
+++ Comentarios.py
@@ -1,2 +1,13 @@
+#xERICK_GEOVVANIx
 #Este es un comentario
 print("Hola mundo!")
+
+print("Hola mundo!") #Este es otro comentario
+
+""""
+Este es
+un
+comentario
+de
+varias lineas
+""""
---
+++ Variables.py
@@ -0,0 +1,33 @@
+#xERICK_GEOVVANIx
+#Creando Variables
+x = 28              
+y = "Erick"
+
+
+x = 28          # x es de tipo entero( int)
+x = "Erick"   # x Ahora es de tipo cadena (str)
+print(x)
+
+#Podemos especificar el tipo de datos
+x = str(3)       # x sera "3" (texto)
+y = int(3)       # y sera 3 (numero)
+z = float(3)    # z sera 3.0 (decimal)
+print(x, y, z)
+
+#Podemos obtener el tipo de dato de una variable haciendo uso de la funcion "type()"
+x = 5
+y = "Erick"
+print(type(x))
+print(type(y))
+
+#Comilla simple o doble sera lo mismo
+x = "Erick "
+y = 'Geovvani'
+print(x + y)
+
+#Python hace la distincion de MAYUSCULAS y minisculuas en variables.
+a = 4
+A = " Erick"
+# A no sobre escribira la variable "a"
+print (a,A)
+
